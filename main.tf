a0bb71617844375f928b43e9c1b5c34107f7fb93
Snekhapugal <SnekhaPuhazendhi>
2025-11-10 17:33:22 +0530

    reserve added

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "azurerm_app_service_plan" "appservice" {
   location            = var.location
   resource_group_name = var.resource_group_name
   kind                = var.kind
+  reserved = true
 
   sku {
     tier = var.sku_tier
